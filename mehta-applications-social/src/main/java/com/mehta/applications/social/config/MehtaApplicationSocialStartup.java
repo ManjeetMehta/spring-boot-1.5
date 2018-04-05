9f1e1ba03392d26b5bfb0b8f80d6fea93e2a72e5
Manjeet Mehta <37746654+ManjeetMehta@users.noreply.github.com>
2018-04-05 23:32:30 +0530

    Update MehtaApplicationSocialStartup.java

--- mehta-applications-social/src/main/java/com/mehta/applications/social/config/MehtaApplicationSocialStartup.java
+++ mehta-applications-social/src/main/java/com/mehta/applications/social/config/MehtaApplicationSocialStartup.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MehtaApplicationSocialStartup {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MehtaApplicationSocialStartup.class, args);
 	}
+	
 }
