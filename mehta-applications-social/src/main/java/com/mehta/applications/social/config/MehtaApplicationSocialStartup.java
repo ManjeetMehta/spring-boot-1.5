9111a75fd3a71594089b6002681bb95c11012670
satyaprakashmehta <mehta.satyaprakash@gmail.com>
2018-04-09 15:35:14 +0530

    test
    test

--- mehta-applications-social/src/main/java/com/mehta/applications/social/config/MehtaApplicationSocialStartup.java
+++ mehta-applications-social/src/main/java/com/mehta/applications/social/config/MehtaApplicationSocialStartup.java
@@ -1,9 +1,13 @@
 package com.mehta.applications.social.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan (basePackages={"com.mehta.applications"})
 public class MehtaApplicationSocialStartup {
 	
 	public static void main(String[] args) {
